f7ff8786eb50acaef578be1013ae7b2120b07fae
HigorSegobia <higor.segobia@gmail.com>
2021-05-07 18:41:53 -0300

    feat(App): Add provider for state management

---
+++ lib/provider/items.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:desafio_flutter/data/data_base.dart';
+import 'package:desafio_flutter/models/item.dart';
+import 'package:flutter/cupertino.dart';
+
+class Items with ChangeNotifier {
+  Map<String, Item> _items = {...DATA_BASE};
+
+  List<Item> get all {
+    return [..._items.values];
+  }
+
+  int get count {
+    return _items.length;
+  }
+
+  Item indexCount(int x) {
+    return _items.values.elementAt(x);
+  }
+
+  Future<void> putItem(Item item) async {
+    if (item == null) {
+      return;
+    }
+
+    if (_items.containsKey(item.id)) {
+      _items.update(
+          item.id,
+          (_) => Item(
+                id: item.id,
+                name: item.name,
+                quantity: item.quantity,
+                price: item.price,
+              ));
+    } else {
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+        id,
+        () => Item(
+          id: id,
+          name: item.name,
+          quantity: item.quantity,
+          price: item.price,
+        ),
+      );
+    }
+
+    notifyListeners();
+  }
+
+  void removeItem(Item item) {
+    if (item != null && item.id != null) {
+      _items.remove(item.id);
+      notifyListeners();
+    }
+  }
+}
